fb1936ae1417324d44e88800e252b0c81ce3433c
yesrin <asjs11@naver.com>
2021-12-07 22:07:14 +0900

    movie컴포넌트 추가하고 배열 내림차순 정렬

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
 
@@ -9,16 +10,37 @@ class App extends React.Component{
   };
   
   getMovies=async()=>{
-    const movies=await axios.get('https://yts-proxy.now.sh/list_movies.json');
-  }
+    const{
+      data:{
+        data:{movies}
+      }
+    }= await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies,isLoding:false});
+  };
 
   componentDidMount(){
     this.getMovies();
   }
 
   render(){
-    const {isLoding}=this.state;
-    return <div>{isLoding ? 'Loding...':'We are ready'}</div>;
+    const {isLoding,movies}=this.state;
+    return (
+      <div>
+        {isLoding ? 'Loding...':movies.map((movie)=>{
+
+          return(
+           <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
