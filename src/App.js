94259cfccd58fbab2dcdb9d13a2450741b682b42
yesrin <asjs11@naver.com>
2021-12-08 21:26:16 +0900

    라우터 v6 오류 수정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import About from './routes/About';
+import Home from './routes/Home';
 
 function App(){
   return (
       <Router>
         <Routes>
-          <Route path="/about" component={About} />
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>} />
         </Routes>
       </Router>
   );
