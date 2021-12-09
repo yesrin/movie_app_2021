51135f8baf1410f1d918b8c007c10bb4091809f3
yesrin <asjs11@naver.com>
2021-12-09 19:40:35 +0900

    Move.js에서 Link를 통해 보내준 데이터를 보여주지 않고 다른값을 보여주는 오류발생

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,8 @@
 import React from 'react';
 
-class Detail extends React.Component {
-  componentDidMount() {
-    const { location, history } = this.props;
-    if (location.state === undefined) {
-      history.push('/');
-    }
-  }
-
-  render() {
-    const { location } = this.props;
-    if (location.state) {
-      return <span>{location.state.title}</span>;
-    } else {
-      return null;
-    }
-  }
+function Detail(props){
+    console.log(props);
+    return <span>hello</span>;
 }
 
 export default Detail;
