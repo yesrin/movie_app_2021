46a2c62979e9bde67ebbe896b40cc93f076b9c2b
yesrin <asjs11@naver.com>
2021-12-09 18:53:50 +0900

    css수정과 Detail.js 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App(){
-  return (
+  
+  return ( 
       <Router>
+        <Navigation />
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>} />
+            <Route path="/movie-detail" element={<Detail/>} />
         </Routes>
       </Router>
   );
